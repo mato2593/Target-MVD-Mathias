68353163117bd213a50b0dbe161f47354bee20ef
Mathias Cabano <cabanomathias@gmail.com>
2017-09-04 14:52:36 -0300

    Add my location button to the map

--- Target-MVD-Mathias/ViewControllers/HomeViewController.swift
+++ Target-MVD-Mathias/ViewControllers/HomeViewController.swift
@@ -14,9 +14,12 @@ class HomeViewController: UIViewController {
   // MARK: Outlets
   @IBOutlet weak var mapViewContainer: UIView!
   @IBOutlet weak var createNewTargetLabel: UILabel!
+  @IBOutlet weak var myLocationButton: UIButton!
   
   // MARK: Variables
   var locationManager = CLLocationManager()
+  var userLocation = CLLocationCoordinate2D()
+  
   lazy var mapView: GMSMapView = {
     // Create a GMSCameraPosition that tells the map to display
     // coordinate -34.906334,-56.184856, at zoom level 16.
@@ -33,6 +36,12 @@ class HomeViewController: UIViewController {
     setupMap()
   }
   
+  // MARK: Actions
+  @IBAction func tapOnMyLocationButton(_ sender: Any) {
+    let camera = GMSCameraPosition.camera(withTarget: userLocation, zoom: 16.0)
+    mapView.animate(to: camera)
+  }
+  
   // MARK: Functions
   private func setLetterSpacing() {
     let defaultSpacing: CGFloat = 1.6
@@ -43,6 +52,7 @@ class HomeViewController: UIViewController {
   private func setupMap() {
     mapView.settings.compassButton = true
     mapViewContainer.addSubview(mapView)
+    mapViewContainer.bringSubview(toFront: myLocationButton)
     
     locationManager.delegate = self
     locationManager.desiredAccuracy = kCLLocationAccuracyBest
@@ -59,6 +69,8 @@ extension HomeViewController: CLLocationManagerDelegate {
     if let coordinates = location?.coordinate {
       locationManager.stopUpdatingLocation()
       
+      userLocation = coordinates
+      
       let camera = GMSCameraPosition.camera(withLatitude: coordinates.latitude,
                                             longitude: coordinates.longitude,
                                             zoom: 16.0)
@@ -67,6 +79,7 @@ extension HomeViewController: CLLocationManagerDelegate {
       locationMarker.icon = #imageLiteral(resourceName: "UserLocation")
       locationMarker.groundAnchor = CGPoint(x: 0.5, y: 0.5)
       locationMarker.isFlat = true
+      locationMarker.opacity = 0.67
       locationMarker.map = mapView
       
       mapView.animate(to: camera)
