4a2b3e4a7adfda6ba714469d32561efcd52024b5
Mathias Cabano <cabanomathias@gmail.com>
2017-09-22 18:33:06 -0300

    Add chats view controller and handle navigation

---
+++ Target-MVD-Mathias/ViewControllers/ChatsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ChatsViewController.swift
+//  Target-MVD-Mathias
+//
+//  Created by Mathias Cabano on 9/22/17.
+//  Copyright © 2017 TopTier labs. All rights reserved.
+//
+
+import UIKit
+
+class ChatsViewController: UIViewController {
+  
+  // MARK: Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  // MARK: Actions
+  @IBAction func tapOnMapBarButtonItem(_ sender: Any) {
+    let homeViewController = UIStoryboard.instantiateViewController(HomeViewController.self)
+    
+    let transition = CATransition()
+    transition.duration = 0.35
+    transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+    transition.type = kCATransitionPush
+    transition.subtype = kCATransitionFromLeft
+    
+    navigationController!.view.layer.add(transition, forKey: kCATransition)
+    navigationController?.pushViewController(homeViewController!, animated: true)
+  }
+}
