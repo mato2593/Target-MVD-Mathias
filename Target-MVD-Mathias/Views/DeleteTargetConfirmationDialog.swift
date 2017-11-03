7b3d54c1bd04cd00ed00f4cbf42d152a2958a634
Mathias Cabano <cabanomathias@gmail.com>
2017-11-03 16:01:06 -0300

    Show delete target confirmation dialog when the user clicks on delete target

--- Target-MVD-Mathias/Views/DeleteTargetConfirmationDialog.swift
+++ Target-MVD-Mathias/Views/DeleteTargetConfirmationDialog.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
-class DeleteTargetConfirmationDialog: UIView {
+protocol DeleteTargetConfirmationDialogDelegate: class {
+  func didTapDeleteTargetButton(target: Target)
+}
+
+class DeleteTargetConfirmationDialog: UIView, Modal {
 
   // MARK: Outlets
   @IBOutlet var contentView: UIView!
@@ -16,13 +20,60 @@ class DeleteTargetConfirmationDialog: UIView {
   @IBOutlet weak var dialogView: UIView!
   
   @IBOutlet weak var titleLabel: UILabel!
-  @IBOutlet weak var targetView: UIView!
-  @IBOutlet weak var targetImageView: UIImageView!
+  @IBOutlet weak var targetTopicView: UIView!
+  @IBOutlet weak var targetTopicImageView: UIImageView!
   @IBOutlet weak var targetTitleLabel: UILabel!
   
   @IBOutlet weak var cancelButton: UIButton!
   @IBOutlet weak var deleteButton: UIButton!
   
-  var target: Target
+  var target: Target?
+  weak var delegate: DeleteTargetConfirmationDialogDelegate?
+ 
+  // MARK: Initializers
+  init(withTarget target: Target) {
+    super.init(frame: UIScreen.main.bounds)
+    loadView()
+    
+    self.target = target
+    setupTopicView()
+    setupLetterSpacing()
+    targetTitleLabel.text = target.title
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    loadView()
+  }
+  
+  // MARK: Functions
+  @IBAction func didTapOnDeleteTargetButton(_ sender: Any) {
+    dismiss(animated: true)
+    delegate?.didTapDeleteTargetButton(target: target!)
+  }
+  
+  @IBAction func didTapOnCancelButton(_ sender: Any) {
+    dismiss(animated: true)
+  }
+  
+  // MARK: Functions
+  private func loadView() {
+    Bundle.main.loadNibNamed("DeleteTargetConfirmationDialog", owner: self, options: nil)
+    addSubview(contentView)
+    contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+  }
+  
+  private func setupTopicView() {
+    targetTopicView.layer.cornerRadius = targetTopicView.frame.size.width / 2
+    targetTopicView.layer.masksToBounds = true
+    targetTopicImageView.sd_setImage(with: target?.topic.icon)
+  }
+  
+  private func setupLetterSpacing() {
+    titleLabel.setSpacing(ofCharacter: 2.0)
+    targetTitleLabel.setSpacing(ofCharacter: 0.5)
+    deleteButton.titleLabel?.setSpacing(ofCharacter: 1.6)
+    cancelButton.titleLabel?.setSpacing(ofCharacter: 1.8)
+  }
   
 }
